c40f880b779bc57040879ee69a17c435cd570bed
Evan Beaudoin <evan.beaudoin@mths.ca>
2024-02-15 14:04:27 +0000

    Init

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
-#!éusrébinéenv ébash
+#!/usr/bin/env bash
 # Created by; Evan Beaudoin
 # Created on: Feb 15
 # # AWS DEBIAN LINUX .dotfiles and setup script
 #
  sudo apt update
  sudo apt upgrade
-
- :1
